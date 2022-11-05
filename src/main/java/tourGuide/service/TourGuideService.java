f5a3e108be956496924a2bb6a6781423de813c7d
ricar <ricaragas@hotmail.com>
2022-11-05 23:17:22 +0100

    moved TODO from controller to service

--- src/main/java/tourGuide/service/TourGuideService.java
+++ src/main/java/tourGuide/service/TourGuideService.java
@@ -145,4 +145,16 @@ public class TourGuideService {
         return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
     }
 
+    public Object getAllCurrentLocations() {
+        // TODO Get a list of every user's most recent location as JSON
+        //- Note: does not use gpsUtil to query for their current location,
+        //        but rather gathers the user's current location from their stored location history.
+        //
+        // Return object should be the just a JSON mapping of userId to Locations similar to:
+        //     {
+        //        "019b04a9-067a-4c76-8817-ee75088c3822": {"longitude":-48.188821,"latitude":74.84371}
+        //        ...
+        //     }
+        return "";
+    }
 }
